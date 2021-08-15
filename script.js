ff68256c7a19f38d5a6c84f936a5b50236a2a254
Maximo-4ever <choripapa04@gmail.com>
2021-08-15 16:07:37 -0500

    Correccion del total de propinas

--- script.js
+++ script.js
@@ -1,125 +1,103 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Variables
-  var frasco = undefined;
-  const inputs = document.querySelectorAll("input");
-  const errorSpan = document.querySelector(".errorSpan");
-  const errorSpan2 = document.querySelector(".errorSpan2");
-  var tipsBox = document.querySelector(".tips-box");
-  const percentage = document.querySelectorAll(".percentage");
-  var selectedPercentage = undefined;
-  var total = document.querySelector(".total__precio");
-  var total2 = document.querySelector(".total__precio2");
-  var btn = document.querySelector(".btn");
+const $inputs = document.querySelectorAll(".input");
+const $errorZero = document.querySelector(".error-Zero");
+const $percentage = document.querySelectorAll(".percentage");
+const tipsBox = document.querySelector(".tips-box");
+const custom = document.getElementById("custom");
+const total = document.querySelector(".total__precio");
+const total2 = document.querySelector(".total__precio2");
+const btn = document.querySelector(".btn");
+var frasco;
+var selectedPercentage;
 
-  //Quitar Error de input
-  inputs.forEach((input) => {
-    input.addEventListener("click", () => {
-      factura.classList.remove("errorPeople");
-      factura.placeholder = "";
-      people.classList.remove("errorPeople");
-      people.placeholder = "";
-    });
+//Quitar Error de input
+$inputs.forEach((input) => {
+  input.addEventListener("click", () => {
+    input.classList.remove("error-Input");
+    input.placeholder = "";
   });
+});
 
-  //Quitar Porcentaje
-  removerActive = () => {
-    if (frasco == undefined) {
-      return;
-    } else {
+//Active Porcentaje
+$percentage.forEach((percentage) => {
+  percentage.addEventListener("click", () => {
+    if (!(percentage.classList.contains("input"))) {
+      custom.value = "";
+      custom.placeholder = "Custom"
+    }
+    percentage.classList.add("active");
+    selectedPercentage = percentage.textContent;
+    selectedPercentage = parseInt(selectedPercentage.replace(/%/, ""));
+    tipsBox.classList.remove("error-Porcentaje");
+    if (frasco !== undefined) {
       frasco.classList.remove("active");
     }
-  };
+    frasco = percentage;
+  });
+});
 
-  //Active Porcentaje
-  percentage.forEach((percentage) => {
-    percentage.addEventListener("click", () => {
-      custom = document.querySelector("#custom");
-      custom.value = "";
-      percentage.classList.add("active");
-      selectedPercentage = percentage.textContent;
-      selectedPercentage = parseInt(selectedPercentage.replace(/%/, ""));
-      tipsBox.classList.remove("errorPorcentaje");
-      removerActive();
-      frasco = percentage;
+// Escucha enviar Info
+btn.addEventListener("click", () => {
+  let factura = document.getElementById("factura");
+  let people = document.getElementById("people");
+  let custom = document.getElementById("custom");
+
+
+  // Validacion de la Factura
+  if (factura.value === "") {
+    factura.classList.add("error-Input");
+    factura.placeholder = "Enter bill";
+    return;
+  }
+  factura.classList.remove("error-Input");
+  factura.placeholder = "";
+
+  if (factura.value == 0) {
+    $errorZero.style.display = "inline";
+    factura.classList.add("error-Input");
+    return;
+  }
+  $errorZero.style.display = "none";
+
+  //Validacion de el Porcentaje
+  if (!selectedPercentage && !custom.value) {
+    tipsBox.classList.add("error-Porcentaje");
+    return;
+  }
+  tipsBox.classList.remove("error-Porcentaje");
+
+  //Validacion de Gente
+  if (people.value === "") {
+    people.classList.add("error-Input");
+    people.placeholder = "Number of people";
+    return;
+  }
+  people.classList.remove("error-Input");
+  people.placeholder = "";
+
+  if (people.value == 0) {
+    $errorZero.style.display = "inline";
+    people.classList.add("error-Zero");
+    return;
+  }
+  $errorZero.style.display = "none";
+
+  // Imprimir propina y total
+  let factura2 = factura.value;
+  factura2 = parseFloat(factura2);
+  let percentage2 = frasco.textContent;
+  percentage2 = parseFloat(percentage2);
+  people = people.value;
+
+  if (custom.value !== "") {
+    custom.addEventListener("click", () => {
+      frasco.classList.remove("active");
     });
-  });
+    percentage2 = parseFloat(custom.value);
+  }
 
-  // Escucha enviar Info
-  btn.addEventListener("click", () => {
-    var factura = document.querySelector("#factura");
-    var people = document.querySelector("#people");
-    custom = document.querySelector("#custom");
-
-    // Validacion de la Factura
-    if (factura.value == "") {
-      console.log("Es necesario seleccionar el dinero en total");
-      factura.classList.add("errorPeople");
-      factura.placeholder = "Enter bill";
-      return;
-    }
-    factura.classList.remove("errorPeople");
-    factura.placeholder = "";
-
-    if (factura.value == 0) {
-      console.log("no puede ser cero");
-      errorSpan.style.display = "inline";
-      factura.classList.add("errorPeople");
-      return;
-    }
-    errorSpan.style.display = "none";
-
-    //Validacion de el Porcentaje
-    if (selectedPercentage == undefined && custom.value == "") {
-      tipsBox.classList.add("errorPorcentaje");
-      console.log("Es necesario seleccionar el selectedPercentage de propina");
-      return;
-    }
-    tipsBox.classList.remove("errorPorcentaje");
-
-    //Validacion de Gente
-    if (people.value == "") {
-      console.log("Es necesario seleccionar en cuanta gente se va a dividir");
-      people.classList.add("errorPeople");
-      people.placeholder = "Number of people";
-      return;
-    }
-    people.classList.remove("errorPeople");
-    people.placeholder = "";
-
-    if (people.value == 0) {
-      console.log("no puede ser cero");
-      errorSpan2.style.display = "inline";
-      people.classList.add("errorPeople");
-      return;
-    }
-    errorSpan2.style.display = "none";
-
-    // Imprimir propina y total
-    factura2 = factura.value;
-    factura2 = parseInt(factura2)
-    percentage2 = frasco.textContent;
-
-    if (custom.value != "") {
-      custom.addEventListener("click", () => {
-        frasco.classList.remove("active");
-      });
-      percentage2 = frasco.value;
-      percentage2 = parseInt(percentage2)
-      people = people.value;
-      tipAmount = factura2 * percentage2 / 100;
-      numeroTotal = (tipAmount / people).toFixed(2)
-      total.textContent = `$${tipAmount / people}.00`;
-      numeroTotal2 = (tipAmount / people * people).toFixed(2)
-      total2.textContent = `$${tipAmount + tipAmount / people}.00`;
-      return;
-    }
-
-    percentage2 = parseInt(percentage2)
-    people = people.value;
-    tipAmount = factura2 * percentage2 / 100;
-    numeroTotal = (tipAmount / people).toFixed(2)
-    total.textContent = `$${numeroTotal}`;
-    numeroTotal2 = (tipAmount / people * people).toFixed(2)
-    total2.textContent = `$${numeroTotal2}`;
-  });
+  tipAmount = (factura2 * percentage2) / 100;
+  numeroTotal1 = (tipAmount / people).toFixed(2);
+  total.textContent = `$${numeroTotal1}`;
+  numeroTotal2 = (tipAmount / people + factura2 / people).toFixed(2);
+  total2.textContent = `$${numeroTotal2}`;
 });
