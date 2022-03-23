1c088e743aec73763f944bc6264039936e1a52da
Maximo-4ever <maximilianocm06@gmail.com>
2022-03-23 14:59:16 -0600

    Refactoriza el codigo y añade mejoras generales

--- script.js
+++ script.js
@@ -1,1 +1,85 @@
-const $inputs=document.querySelectorAll(".input");const $errorZero=document.querySelector(".error-Zero");const $percentage=document.querySelectorAll(".percentage");const tipsBox=document.querySelector(".tips-box");const custom=document.getElementById("custom");const total=document.querySelector(".total__precio");const total2=document.querySelector(".total__precio2");const btn=document.querySelector(".btn");var frasco;var selectedPercentage; $inputs.forEach((input)=>{input.addEventListener("click",()=>{input.classList.remove("error-Input");input.placeholder=""})}); $percentage.forEach((percentage)=>{percentage.addEventListener("click",()=>{if(!percentage.classList.contains("input")){custom.value="";custom.placeholder="Custom"}percentage.classList.add("active");selectedPercentage=percentage.textContent;selectedPercentage=parseInt(selectedPercentage.replace(/%/,""));tipsBox.classList.remove("error-Porcentaje");if(frasco!==undefined){frasco.classList.remove("active")}frasco=percentage})}); btn.addEventListener("click",()=>{let factura=document.getElementById("factura");let people=document.getElementById("people");let custom=document.getElementById("custom"); if(factura.value===""){factura.classList.add("error-Input");factura.placeholder="Enter bill";return}factura.classList.remove("error-Input");factura.placeholder="";if(factura.value==0){$errorZero.style.display="inline";factura.classList.add("error-Input");return}$errorZero.style.display="none"; if(!selectedPercentage&&!custom.value){tipsBox.classList.add("error-Porcentaje");return}tipsBox.classList.remove("error-Porcentaje"); if(people.value===""){people.classList.add("error-Input");people.placeholder="Number of people";return}people.classList.remove("error-Input");people.placeholder="";if(people.value==0){$errorZero.style.display="inline";people.classList.add("error-Zero");return}$errorZero.style.display="none"; let factura2=factura.value;factura2=parseFloat(factura2);let percentage2=frasco.textContent;percentage2=parseFloat(percentage2);people=people.value;if(custom.value!==""){custom.addEventListener("click",()=>{frasco.classList.remove("active")});percentage2=parseFloat(custom.value)}tipAmount=(factura2*percentage2)/100;numeroTotal1=(tipAmount/people).toFixed(2);total.textContent=`$${numeroTotal1}`;numeroTotal2=(tipAmount /people+factura2/ people).toFixed(2);total2.textContent=`$${numeroTotal2}`});
+const $allInputs = document.querySelectorAll(".input");
+const $inputFacture = document.querySelector("#factura");
+const $inputCustomTip = document.querySelector("#custom");
+const $inputPeople = document.querySelector("#people");
+const $errorZero = document.querySelector(".error-Zero");
+
+const tipsBox = document.querySelector(".selectTip");
+const btnReset = document.querySelector(".total__btn");
+let selectedPercentage = 15;
+
+$allInputs.forEach((input) => {
+  input.addEventListener("click", () => {
+    input.classList.remove("error-Input");
+  });
+  input.addEventListener("input", () => calculateTip());
+});
+
+tipsBox.addEventListener("click", (evt) => {
+  const { target } = evt;
+  let listSiblings = target.parentElement.children;
+  selectedPercentage = "";
+  listSiblings = Array.from(listSiblings);
+  if (target.classList.contains("input")) {
+    // Si el porcentaje es personalizado:
+    $inputCustomTip.addEventListener("input", (evt) => {
+      selectedPercentage += evt.data;
+    });
+  } else if (target.classList.contains("selectTip__percentage")) {
+    $inputCustomTip.value = "";
+    selectedPercentage = parseInt(target.textContent.replace(/%/, ""));
+  }
+  listSiblings.forEach((element) => element.classList.remove("active"));
+  target.classList.add("active");
+});
+
+const validations = (inputFacture, inputPeople) => {
+  if (inputFacture.value == 0) {
+    $errorZero.style.display = "inline";
+    inputFacture.classList.add("error-Input");
+    return;
+  }
+  if (inputPeople.value === "") {
+    inputPeople.classList.add("error-Input");
+    inputPeople.placeholder = "Number of people";
+    return;
+  }
+  $errorZero.style.display = "none";
+  inputPeople.classList.remove("error-Input");
+  inputPeople.placeholder = "";
+};
+
+const printResult = (factura, percentage, people) => {
+  const total1 = document.querySelector(".total__precio");
+  const total2 = document.querySelector(".total__precio2");
+  if ($inputCustomTip.value !== "") {
+    percentage = parseFloat($inputCustomTip.value);
+  }
+  tipAmount = (factura * percentage) / 100;
+  // Print
+  numeroTotal1 = (tipAmount / people).toFixed(2);
+  total1.textContent = `$${numeroTotal1}`;
+  numeroTotal2 = (tipAmount / people + factura / people).toFixed(2);
+  total2.textContent = `$${numeroTotal2}`;
+};
+
+const calculateTip = () => {
+  if ($inputFacture.value === "") {
+    $inputFacture.classList.add("error-Input");
+    $inputFacture.placeholder = "0";
+    return;
+  }
+  $inputFacture.classList.remove("error-Input");
+  $inputFacture.placeholder = "";
+
+  validations($inputFacture, $inputPeople);
+
+  let factura = parseFloat($inputFacture.value);
+  let percentage = parseFloat(selectedPercentage);
+  let people = parseFloat($inputPeople.value);
+  printResult(factura, percentage, people);
+};
+
+btnReset.addEventListener("click", () => {
+  $inputFacture.value = 0;
+});
